8ed4d9a7052995fc446f15a58ea140aa809c814a
Lee Seul <love.and.seul@gmail.com>
2025-09-10 21:27:06 +0900

    feat: header component - add date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <div className="">
-      <div className="text-4xl ">Today, what to do?</div>
+      <div className="text-4xl text-center text-indigo-200">
+        Today is `{new Date().toDateString()}`
+      </div>
     </div>
   );
 }
