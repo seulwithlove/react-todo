5c7183e5cf815c73dc52fef99a9db386d225cb65
Lee Seul <love.and.seul@gmail.com>
2025-09-11 15:08:07 +0900

    style: update header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,8 @@ export default function Header() {
   return (
     <div className="">
       <div className="text-4xl text-center text-indigo-200">
-        Today is `{new Date().toDateString()}`
+        <section>Today</section>
+        <section>`{new Date().toDateString()}`</section>
       </div>
     </div>
   );
